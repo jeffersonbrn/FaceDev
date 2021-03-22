8e19bfc39d586fe8172ed153164e668f34c2fa1a
Bruno Reis <jeffersonbrn@live.com>
2021-03-22 13:44:16 -0300

    Demais contribuições

--- src/pages/home/components/header.js
+++ src/pages/home/components/header.js
@@ -1,20 +1,40 @@
 import React from "react";
+import { Button } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+// import '../style.css'
+
+const useStyle = makeStyles({
+  appBar:{
+    boxShadow: 'none',
+  },
+  img:{
+    maxHeight: 55,
+  }
+});
 
 function Header() {
+  const classes = useStyle();
   return (
-    <header className="header">
-      <div className="toolbar">
-        <div>
-          <span>Facedev</span>
+    <AppBar position="fixed" color="inherit" className={classes.appBar} >
+      <Toolbar>
+        <img className={classes.img} src="/images/logo.bmp" alt="Logo"/>
+
+        {/* <div>
+          <a href="/">FaceDev</a>
+          <input type="text"/>
         </div>
         <div>
-          <button>Novo Post</button>
+          <Button color="primary" variant="contained" >
+              Novo Post
+            </Button>
           <span>imag1</span>
           <span>imag2</span>
-        </div>
-      </div>
-    </header>
-  );
+        </div> */}
+      </Toolbar>
+    </AppBar>
+  )
 }
 
 export default Header;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,33 @@
 import React from "react";
-import "./style.css";
-import Header from './components/header'
+import { makeStyles } from '@material-ui/core/styles';
+
+import Header from './components/Header';
+import Feed from './components/Feed';
+import NavBar from './components/NavBar';
+
+// import "./style.css";
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    flexDirection: 'column'
+  }, 
+  main: {
+    height: '100vh',
+    display: 'flex',
+    width: '1200px',
+    margin: '0 auto',
+  }
+})
+
 function Home() {
-  return (
-    <div>
+  const classes = useStyles();
+    return (
+    <div className={classes.root} >
         <Header/>
-      <main className="main">
-        <div className="navbar">navbar</div>
-        <div className="feed">feed Space</div>
+      <main className={classes.main}>
+        <NavBar />
+        <Feed />
       </main>
     </div>
   );
