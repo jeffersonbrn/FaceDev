4f2344a9295a5a52547db3610d7b616ca9c2a8ea
Bruno Reis <jeffersonbrn@live.com>
2021-03-28 17:56:43 -0300

    Parou no momento em que iria incluir o formulario via grid e box

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
 
-function SignIn () {
-    return (
-        <h1>Pagina de login</h1>
-    )
+const useStyles = makeStyles((theme) => ({
+  root: {
+
+  },
+}));
+
+function SignIn() {
+  const classes = useStyles();
+  return (
+
+
+
+    /*
+    // Div representa o flex container
+    <div className={classes.root}>
+      <div className={classes.left}>
+        <Typography style={{ color: "#fff", fontSize: 35, lineHeight: "45px" }}>
+          <strong>Simplificando a forma de como conectar uma empresa</strong>
+        </Typography>
+        <Typography
+          variant="body2"
+          style={{
+            color: "rgb(255,255,255, 0.7",
+            marginTop: 30,
+            fontSize: 15,
+            lineHeight: "30px",
+          }}
+        >
+          Gerenciamento de conteúdo, informações e detalhes sobre nosso dia a
+          dia em uma rede corporativa.
+        </Typography>
+      </div>
+      <div className={classes.right}>
+          <form className={classes.form} > 
+            <h4>Acesse</h4>
+            <input type="text" />
+            <input type="text" />
+          </form>
+      </div>
+    </div> */
+  );
 }
 
 export default SignIn;
