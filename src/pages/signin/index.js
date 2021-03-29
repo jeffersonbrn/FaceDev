3d468be385a0d7c23f3782262821ad6c9564dd57
Bruno Reis <jeffersonbrn@live.com>
2021-03-29 13:44:52 -0300

    aula 29 de março

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -1,50 +1,100 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, rgbToHex } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
+import Avatar from "@material-ui/core/Avatar";
+import LockOutlinedIcon from '@material-ui/icons/LockOpenOutlined';
+import TextField from '@material-ui/core/TextField';
+import { Button } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    height: '100vh'
 
   },
+  image:{
+    backgroundImage: 'url(/images/background.png)',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'none'
+  },
+  avatar:{
+    background: theme.palette.secondary.main,
+    marginBottom: theme.spacing(1)
+  }
 }));
 
 function SignIn() {
   const classes = useStyles();
-  return (
 
-
-
-    /*
-    // Div representa o flex container
-    <div className={classes.root}>
-      <div className={classes.left}>
-        <Typography style={{ color: "#fff", fontSize: 35, lineHeight: "45px" }}>
-          <strong>Simplificando a forma de como conectar uma empresa</strong>
-        </Typography>
-        <Typography
-          variant="body2"
-          style={{
-            color: "rgb(255,255,255, 0.7",
-            marginTop: 30,
-            fontSize: 15,
-            lineHeight: "30px",
-          }}
+  return(
+    <Grid container className={classes.root} >
+      <Grid 
+        item 
+        container 
+        direction="column"
+        justify="center"
+        alignItems="center"
+        md={7} 
+        className={classes.image}
         >
-          Gerenciamento de conteúdo, informações e detalhes sobre nosso dia a
-          dia em uma rede corporativa.
+        <Typography style={{color:'#fff', fontSize: 35, lineHeight: '45px'}} >
+          <strong>Simplificando a vida de contadores e empresas</strong>
         </Typography>
-      </div>
-      <div className={classes.right}>
-          <form className={classes.form} > 
-            <h4>Acesse</h4>
-            <input type="text" />
-            <input type="text" />
+        <Typography variant='body2' style={{color:'rgb(255, 255, 255, 0.7)', marginTop:30, fontSize: 15, lineHeight: '30px'}} >
+          <strong>Compartilhando seu conhecimento com sua equipe é algo incrivel de se fazer</strong>
+        </Typography>
+      </Grid>
+      <Grid item md={5} >
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          m={8}
+        >
+          <Avatar className={classes.avatar} >
+            <LockOutlinedIcon></LockOutlinedIcon>
+          </Avatar>
+          <Typography variant="h5" >
+            Acesso
+          </Typography>
+          <form>
+            <TextField 
+              variant='outlined'
+              margin='normal'
+              required
+              fullWidth
+              id='email'
+              label='E-mail'
+              name='email'
+              autoComplete="Email"
+              autoFocus
+            />
+            <TextField 
+              variant='outlined'
+              margin='normal'
+              required
+              fullWidth
+              name="password"
+              label="Senha"
+              type="password"
+              id='password'
+              autoComplete="current-password"
+            />
+            <Button 
+            fullWidth
+            variant="contained"
+            color="secondary"
+          >
+            Entrar
+          </Button>
           </form>
-      </div>
-    </div> */
-  );
+          
+        </Box>
+      </Grid>
+    </Grid>
+  )
 }
 
 export default SignIn;
